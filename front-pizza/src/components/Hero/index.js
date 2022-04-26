dd0f3f049573eebc56ca6dcf7465606b245a68c9
Jonghan Park <jonghan.park@edu.sait.ca>
2022-04-26 17:18:16 -0600

    Update Main page

--- front-pizza/src/components/Hero/index.js
+++ front-pizza/src/components/Hero/index.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import NavBar from '../Navbar'
-import {HeroContainer} from '../Hero/HeroElements'
+import {HeroContainer, HeroContent, HeroItems, HeroH1, HeroP, HeroBtn} from '../Hero/HeroElements'
 
 const Hero = () => {
   return (
     <HeroContainer>
       <NavBar />
+      <HeroContent>
+        <HeroItems>
+          <HeroH1>Greatest Pizza Ever</HeroH1>
+          <HeroP>Ready in 60 seconds</HeroP>
+          <HeroBtn>Place Order</HeroBtn>
+        </HeroItems>
+      </HeroContent>
     </HeroContainer>
   )
 }
