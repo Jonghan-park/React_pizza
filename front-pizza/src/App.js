9e345d4f5658d58d733b036b13617988a066bc40
Jonghan Park <jonghan.park@edu.sait.ca>
2022-04-28 13:24:05 -0600

    Update Scroll to top

--- front-pizza/src/App.js
+++ front-pizza/src/App.js
@@ -6,12 +6,14 @@ import Products from './components/Products';
 import {productData, productDataTwo} from './components/Products/data'
 import Feature from './components/Feature';
 import Footer from './components/Footer';
+import ScrollToTop from './components/ScrollToTop';
 
 function App() {
   
   return (
     <Router>
       <GlobalStyle />
+      <ScrollToTop />
       <Hero />
       <Products heading='Choose your favorite' data={productData} />
       <Feature />
--- front-pizza/src/components/ScrollToTop/index.js
+++ front-pizza/src/components/ScrollToTop/index.js
@@ -1,13 +1,40 @@
-import React from 'react'
-import { ScrollToTopContainer, ScrollToTopIconContainer } from './ScrollToTopElements'
-import { ScrollToTopIcon } from 'react-icons/fa'
+import React, { useState, useEffect } from 'react'
+import { ScrollToTopContainer, ScrollToTopIconContainer, ScrollToTopIcon } from './ScrollToTopElements'
+import { FaAngleUp } from 'react-icons/fa'
 
 const ScrollToTop = () => {
+  const [showTopBtn, setShowTopBtn] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      if(window.scrollY > 400){
+        setShowTopBtn(true);
+      }else{
+        setShowTopBtn(false);
+      }
+    });
+  }, []);
+
+  const goToTop = () =>{
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
+
   return (
     <ScrollToTopContainer>
-      <ScrollToTopIconContainer>
-      <ScrollToTopIcon />
+      {" "}
+      { showTopBtn && (
+      <ScrollToTopIconContainer onClick={goToTop}>
+      <ScrollToTopIcon>
+      
+        <FaAngleUp />
+        
+      </ScrollToTopIcon>
       </ScrollToTopIconContainer>
+      )} {" "}
     </ScrollToTopContainer>
   )
 }
