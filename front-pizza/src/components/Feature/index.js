4de7dc3676ab686a79b5fdec1e7ef7aabdfe144c
Jonghan Park <jonghan.park@edu.sait.ca>
2022-04-28 10:21:01 -0600

    Update feature element

--- front-pizza/src/components/Feature/index.js
+++ front-pizza/src/components/Feature/index.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import {FeatureContainer, FetureButton} from './FeatureElements'
+import {FeatureContainer, FeatureButton} from './FeatureElements'
 
 const Feature = () => {
   return (
     <FeatureContainer>
       <h1>Pizza of the day</h1>
       <p>Truffle alfredo sauce topped with 24 carat gold dust.</p>
-      <FetureButton>Order Now</FetureButton>
+      <FeatureButton>Order Now</FeatureButton>
     </FeatureContainer>
   )
 }
