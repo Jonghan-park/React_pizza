dc10480dc31846f9a86da8f316f3809ea28681a6
Jonghan Park <jonghan.park@edu.sait.ca>
2022-04-28 10:10:44 -0600

    Create feature

---
+++ front-pizza/src/components/Feature/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {FeatureContainer, FetureButton} from './FeatureElements'
+
+const Feature = () => {
+  return (
+    <FeatureContainer>
+      <h1>Pizza of the day</h1>
+      <p>Truffle alfredo sauce topped with 24 carat gold dust.</p>
+      <FetureButton>Order Now</FetureButton>
+    </FeatureContainer>
+  )
+}
+
+export default Feature
